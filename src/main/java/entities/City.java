c374fc843466ead215b58b72c6d9d1d5503ac6b5
Marcus Hindsbøl <cph-1054@cphbusiness.dk>
2023-04-19 12:12:30 +0200

    Testing workflow

---
+++ src/main/java/entities/City.java
@@ -0,0 +1,20 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
